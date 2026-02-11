cfb16c0ad712404fdbc7c7f5c6346f4b12472dae
vishnuvardhan2114 <vishnuvardhan14102002@gmail.com>
2026-02-11 15:47:32 +0530

    refactor: remove redundant CORS middleware from app.js to streamline request handling

--- app.js
+++ app.js
@@ -267,11 +267,6 @@ app.use(cors({
   credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
 app.options('*', (req, res) => {
   res.status(200).end();
 });
